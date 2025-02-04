0a5f1d8257a744d3b0c2ac73e5754f072994bff5
unknown <chatterjee.mainak15@gmail.com>
2025-02-04 12:47:00 +0530

    Updated the packages

--- index.js
+++ index.js
@@ -6,40 +6,32 @@ import boxen from "boxen";
 const options = {
     padding: 1,
     width: 60,
-    title: "Hello there!",
+    title: "Hello there! I am Mainak",
     titleAlignment: "center",
     borderStyle: "single",
     borderColor: "#008080",
 };
 
 const data = {
-    intro: chalk.white.bold("I am Mainak, your friendly neighborhood coder, focused on providing Solutions"),
-    work: chalk.white("Interned at") + chalk.blue(" EVE Healthcare , TeachForIndia , HMT Architects"),
-    twitter: chalk.gray("https://twitter.com/") + chalk.cyan("MainakC81591663"),
+    intro: chalk.white.bold("Skilled in Angular and Java Spring Boot development, with experience in databases, Linux systems, DevOps tools, and applications of Generative AI and Agentic AI. For projects, visit my GitHub. With a B.Tech in Computer Science and Engineering degree , I bring a strong foundation in Software development, Data Engineering , and a proven track record of impactful projects and internships, including leading a winning hackathon project and hosting corporate websites."),
+    twitter: chalk.cyan("https://callmainak.vercel.app/"),
     github: chalk.gray("https://github.com/") + chalk.green("mainak0907"),
     linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("mainakc"),
-    web: chalk.cyan.underline("https://shorturl.at/dehyK"),
-    labelWork: chalk.bgWhite.black.bold("       Work"),
-    labelTwitter: chalk.bgWhite.black.bold("    Twitter"),
+    labelTwitter: chalk.bgWhite.black.bold("    Portfolio"),
     labelGitHub: chalk.bgWhite.black.bold("     GitHub"),
     labelLinkedIn: chalk.bgWhite.black.bold("   LinkedIn"),
-    labelWeb: chalk.bgWhite.black.bold("        Resume"),
 };
 
 const newline = "\n";
 const introduction = `${data.intro}`;
-const working = `${data.labelWork}  ${data.work}`;
 const twittering = `${data.labelTwitter}  ${data.twitter}`;
 const githubing = `${data.labelGitHub}  ${data.github}`;
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`;
-const webing = `${data.labelWeb}  ${data.web}`;
 
 const output =
     introduction + newline + newline + newline +
-    working + newline +
     githubing + newline +
     linkedining + newline +
-    twittering + newline +
-    webing ;
+    twittering + newline ;
 
 console.log(chalk.white(boxen(output, options)));
